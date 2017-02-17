3493930d04d1f77d5fcf76aa53a82cb1ef717d43
海明威 <microwind.x@gmail.com>
2017-02-18 02:34:46 +0800

    critical section
    s

--- lasslive/lasslive.ino
+++ lasslive/lasslive.ino
@@ -1,10 +1,15 @@
 volatile static int pm1,pm25,pm10,bme280_p,bme280_h;
 static float bme280_t;
+uint32_t sema;
 #include "live.h";
+Adafruit_BME280 bme;
 
 void console_print(const void *argument){
+  wdt_enable(5000);
   while(1){
+    os_semaphore_wait(sema,0xFFFFFFFF);
     Serial.println(String("BME280:") + bme280_t +" C " + bme280_h + " % " + bme280_p/100 + " pa, PM: " + pm1 + " ," + pm25 + " ," + pm10 );
+    os_semaphore_release(sema);
     os_thread_yield();
     delay(1000);
     
@@ -12,12 +17,14 @@ void console_print(const void *argument){
 }
 
 void read_bme(const void *argument){
-  Adafruit_BME280 bme;
+  
   bme.begin();
   while(1){
+    os_semaphore_wait(sema,0xFFFFFFFF);
     bme280_t=bme.readTemperature();
     bme280_p=bme.readPressure();
     bme280_h=bme.readHumidity();
+    os_semaphore_release(sema);
     wdt_reset();
     os_thread_yield();
     delay(2000);
@@ -26,11 +33,13 @@ void read_bme(const void *argument){
 
 void read_g3(const void *argument){
   while(1){
+    os_semaphore_wait(sema,0xFFFFFFFF);
     uint8_t buf[32];
     uint8_t c = 0;
     int idx = 0;
     memset(buf, 0, 32);
-    while (true) {
+    uint32_t timeout=millis();
+    while (millis() - timeout < 1000) {
       while (c != 0x42) {
         while (!Serial1.available());
         c = Serial1.read();
@@ -42,14 +51,17 @@ void read_g3(const void *argument){
         buf[idx++] = 0x4d;
         break;
       }
+      while (idx != 32) {
+        while(!Serial1.available());
+        buf[idx++] = Serial1.read();
+      }
+      pm1 = ( buf[10] << 8 ) | buf[11];
+      pm25 = ( buf[12] << 8 ) | buf[13];
+      pm10 = ( buf[14] << 8 ) | buf[15];
     }
-    while (idx != 32) {
-      while(!Serial1.available());
-      buf[idx++] = Serial1.read();
-    }
-    pm1 = ( buf[10] << 8 ) | buf[11];
-    pm25 = ( buf[12] << 8 ) | buf[13];
-    pm10 = ( buf[14] << 8 ) | buf[15];
+    
+    
+    os_semaphore_release(sema);
     os_thread_yield();
     delay(3000);
   }
@@ -59,15 +71,11 @@ void setup() {
   //create many thread
   Serial1.begin(9600);
   Serial.begin(38400);
-  wdt_enable(5000);
-  os_thread_create(read_g3, NULL, OS_PRIORITY_NORMAL, 1024);
-  os_thread_create(read_bme, NULL, OS_PRIORITY_NORMAL, 1024);
-  os_thread_create(console_print, NULL, OS_PRIORITY_LOW, 1024);
+  sema = os_semaphore_create(1);
+  os_thread_create(read_g3, NULL, OS_PRIORITY_HIGH, 1024);
+  os_thread_create(read_bme, NULL, OS_PRIORITY_REALTIME, 1024);
+  os_thread_create(console_print, NULL, OS_PRIORITY_HIGH, 1024);
 }
-
-
-
-
 
 
 
