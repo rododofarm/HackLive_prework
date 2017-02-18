778c15eefa2eef8e1588f796984b510f5458ee13
海明威 <microwind.x@gmail.com>
2017-02-18 09:56:35 +0800

    8710 bug change 8195

--- lasslive/lasslive.ino
+++ lasslive/lasslive.ino
@@ -1,14 +1,13 @@
-volatile static int pm1,pm25,pm10,bme280_p,bme280_h;
+volatile static int pm25,pm10,bme280_p,bme280_h;
 static float bme280_t;
 uint32_t sema;
 #include "live.h";
 Adafruit_BME280 bme;
 
 void console_print(const void *argument){
-  wdt_enable(5000);
+  
   while(1){
     os_semaphore_wait(sema,0xFFFFFFFF);
-    Serial.println(String("BME280:") + bme280_t +" C " + bme280_h + " % " + bme280_p/100 + " pa, PM: " + pm1 + " ," + pm25 + " ," + pm10 );
     os_semaphore_release(sema);
     os_thread_yield();
     delay(1000);
@@ -17,8 +16,6 @@ void console_print(const void *argument){
 }
 
 void read_bme(const void *argument){
-  
-  bme.begin();
   while(1){
     os_semaphore_wait(sema,0xFFFFFFFF);
     bme280_t=bme.readTemperature();
@@ -33,66 +30,75 @@ void read_bme(const void *argument){
 
 void read_g3(const void *argument){
   while(1){
-    os_semaphore_wait(sema,0xFFFFFFFF);
-    uint8_t buf[32];
-    uint8_t c = 0;
-    int idx = 0;
-    memset(buf, 0, 32);
-    uint32_t timeout=millis();
-    while (millis() - timeout < 1000) {
-      while (c != 0x42) {
-        while (!Serial1.available());
-        c = Serial1.read();
+      os_semaphore_wait(sema,0xFFFFFFFF);
+      bme280_t=bme.readTemperature();
+      bme280_p=bme.readPressure();
+      bme280_h=bme.readHumidity();
+      
+      wdt_reset();
+      unsigned long timeout = millis();
+      int count=0;
+      byte incomeByte[24];
+      boolean startcount=false;
+      byte data;
+      while (1){
+        if((millis() -timeout) > 1000) {    
+          Serial.println("[G3-ERROR-TIMEOUT]");
+          //#TODO:make device fail alarm message here
+          break;
+        }
+        if(Serial1.available()){
+          data=Serial1.read();
+        if(data==0x42 && !startcount){
+          startcount = true;
+          count++;
+          incomeByte[0]=data;
+        }else if(startcount){
+          count++;
+          incomeByte[count-1]=data;
+          if(count>=24) {break;}
+         }
+        }
       }
-      while (!Serial1.available());
-      c = Serial1.read();
-      if (c == 0x4d) {
-        buf[idx++] = 0x42;
-        buf[idx++] = 0x4d;
-        break;
+      unsigned int calcsum = 0; // BM
+      unsigned int exptsum;
+      for(int i = 0; i < 22; i++) {
+        calcsum += (unsigned int)incomeByte[i];
       }
-      while (idx != 32) {
-        while(!Serial1.available());
-        buf[idx++] = Serial1.read();
+      
+      exptsum = ((unsigned int)incomeByte[22] << 8) + (unsigned int)incomeByte[23];
+      if(calcsum == exptsum) {
+        pm25 = ((unsigned int)incomeByte[12] << 8) + (unsigned int)incomeByte[13];
+        //PM10
+        pm10 = ((unsigned int)incomeByte[14] << 8) + (unsigned int)incomeByte[15];
+      } else {
+        Serial.println("#[exception] PM2.5 Sensor CHECKSUM ERROR!");
       }
-      pm1 = ( buf[10] << 8 ) | buf[11];
-      pm25 = ( buf[12] << 8 ) | buf[13];
-      pm10 = ( buf[14] << 8 ) | buf[15];
-    }
-    
-    
-    os_semaphore_release(sema);
-    os_thread_yield();
-    delay(3000);
-  }
+      os_semaphore_release(sema);
+      delay(3000);
+   }
 }
 
 void setup() {
+  wdt_enable(8000);
   //create many thread
   Serial1.begin(9600);
   Serial.begin(38400);
+  bme.begin();
   sema = os_semaphore_create(1);
   os_thread_create(read_g3, NULL, OS_PRIORITY_HIGH, 1024);
-  os_thread_create(read_bme, NULL, OS_PRIORITY_REALTIME, 1024);
-  os_thread_create(console_print, NULL, OS_PRIORITY_HIGH, 1024);
+  //os_thread_create(read_bme, NULL, OS_PRIORITY_REALTIME, 1024);
+  //os_thread_create(console_print, NULL, OS_PRIORITY_HIGH, 1024);
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-void loop() {delay(1000);}
+void loop() {
+  os_semaphore_wait(sema,0xFFFFFFFF);
+  Serial.println(String("BME280:") + bme280_t +" C " + bme280_h + " % " + bme280_p/100 + " pa, PM: " + pm25 + " ," + pm10 );
+  os_semaphore_release(sema);
+  delay(1000);
+ }
 // 你只能寫到 87行不能再多了 ....
 //====No Code==== 
 
